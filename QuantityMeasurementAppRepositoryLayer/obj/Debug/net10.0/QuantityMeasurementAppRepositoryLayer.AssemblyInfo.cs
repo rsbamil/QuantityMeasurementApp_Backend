c84fbdf3fd3ecf8bd33e2f47aad9eedf88df6322
Rohit Kumar <rsbamil66@gamil.com>
2026-04-13 15:10:50 +0530

    [Rohit Kumar]

--- QuantityMeasurementAppRepositoryLayer/obj/Debug/net10.0/QuantityMeasurementAppRepositoryLayer.AssemblyInfo.cs
+++ QuantityMeasurementAppRepositoryLayer/obj/Debug/net10.0/QuantityMeasurementAppRepositoryLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurementAppRepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52ef42250900c828d3e61f5982d48b514e38fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurementAppRepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurementAppRepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
